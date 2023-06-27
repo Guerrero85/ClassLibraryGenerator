cee6f187f45bb9ef8a77aa91c4f99d525de6c0ab
Cesar Guerrero <Cessar.graux@gmail.com>
2023-06-27 09:30:27 -0400

    cambio y actualizacion en Generator

--- ClassLibraryGenerator/Generator/Generator.cs
+++ ClassLibraryGenerator/Generator/Generator.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace ClassLibraryGenerator.Generator
 {
     public class Generator
     {
-        public List<string> Content { get; set;}
-        public string Path { get; set;}
-        public TypeFormat Format { get; set;}
-        public TypeCharacter Character { get; set;} 
-        public Generator() { }
+        public List<string> Content { get; set; }
+        public string Path { get; set; }
+        public TypeFormat Format { get; set; }
+        public TypeCharacter Character { get; set; }
+        public void Save()
+        {
+            string result = "";
+        }
+        private string GentJson() => JsonSerializer.Serialize(Content);
+        private string GetPipe() => Content.Aggregate((accumulator, current) => accumulator + "|" + current);
     }
 }
