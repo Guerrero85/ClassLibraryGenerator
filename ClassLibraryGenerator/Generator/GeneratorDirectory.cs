8908759e732dfde6da4a7fe6130a142091bda991
Cesar Guerrero <Cessar.graux@gmail.com>
2023-06-27 10:15:34 -0400

    Actualizacion

--- ClassLibraryGenerator/Generator/GeneratorDirectory.cs
+++ ClassLibraryGenerator/Generator/GeneratorDirectory.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace ClassLibraryGenerator.Generator
 {
-    internal class GeneratorDirectory
+    public class GeneratorDirectory
     {
+        private IBuilderGenerator _builderGenerator;
+
+        public GeneratorDirectory(IBuilderGenerator generatorBuilder)
+        {
+            SetBuilder(generatorBuilder);
+        }
+        public void SetBuilder(IBuilderGenerator builderGenerator) => _builderGenerator = builderGenerator;
+        public void CreateSimpleJson(List<string> content, string path)
+        {
+            _builderGenerator.Reset();
+            _builderGenerator.SetContent(content);
+            _builderGenerator.SetPath(path);
+            _builderGenerator.SetFormat(TypeFormat.Json);
+        }
+        public void CreateSimplePipe(List<string> content, string path)
+        {
+            _builderGenerator.Reset();
+            _builderGenerator.SetContent(content);
+            _builderGenerator.SetPath(path);
+            _builderGenerator.SetFormat(TypeFormat.Pipes);
+            _builderGenerator.SetCharacter(TypeCharacter.Uppercase);
+        }
     }
 }
